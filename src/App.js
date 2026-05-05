9811f02e83290978a3d66f3a0b6b0cb3d150657d
Your Name <oolalonpe@gmail.com>
2026-05-05 17:00:25 +0200

    imported bootstrap on the app

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <h1 className="App">React-weather-app-project</h1>;
+  return (
+    <div className="App">
+      {" "}
+      <div className="container">
+        {" "}
+        <h1>React-weather-app-project</h1>{" "}
+        <button type="button" className="btn btn-primary">
+          login
+        </button>
+      </div>
+    </div>
+  );
 }
