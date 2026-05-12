f8d337216b3ab08e939333352db0868d877e3aa7
Your Name <oolalonpe@gmail.com>
2026-05-12 21:13:52 +0200

    updated forecast emoji url

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -23,11 +23,15 @@ export default function WeatherForcast(props) {
     console.log(forecast);
     let minTemperature = Math.round(forecast.daily[0].temperature.minimum);
     let maxTemperature = Math.round(forecast.daily[0].temperature.maximum);
+    let iconUrl = forecast.daily[0].condition.icon_url;
     return (
       <div className="WeatherForcast">
         <ul className=" forecast-row row">
           <li>Mon </li>
-          <li className="forecast-icon">❄️</li>{" "}
+          <li className="forecast-icon">
+            {" "}
+            <img src={iconUrl} alt="weather-emoji" />{" "}
+          </li>{" "}
           <li className="forecast-temperatures">
             <span className="forecast-temperature-max">{maxTemperature}°</span>
             <span className="forecast-temperature-min">{minTemperature}°</span>
