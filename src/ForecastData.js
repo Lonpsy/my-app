060798a7fd7cbe2544108b30b5a33a691a897e62
Your Name <oolalonpe@gmail.com>
2026-05-13 11:37:21 +0200

    formated the date
    updated the date to make it readable

--- src/ForecastData.js
+++ src/ForecastData.js
@@ -4,10 +4,17 @@ export default function ForecastData(props) {
   let minTemperature = Math.round(props.data.daily[0].temperature.minimum);
   let maxTemperature = Math.round(props.data.daily[0].temperature.maximum);
   let iconUrl = props.data.daily[0].condition.icon_url;
+  let date = new Date(props.data.daily[0].time * 1000);
+  function day(date) {
+    let actualDay = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[actualDay];
+  }
+
   return (
     <div>
       <ul className=" forecast-row row">
-        <li>Mon </li>
+        <li>{day(date)}</li>
         <li className="forecast-icon">
           {" "}
           <img src={iconUrl} alt="weather-emoji" />{" "}
